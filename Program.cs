71e343c99bd041b36ddd3853d3510ab2cd17085c
KristofferHode <kristoffer9.kodehode@gmail.com>
2025-10-31 09:33:17 +0100

    final final commit

--- Program.cs
+++ Program.cs
@@ -11,6 +11,22 @@ public class Program
         {
             Console.WriteLine("> ");
             string? input = Console.ReadLine();
+        if (input == "ls")
+        {
+            magnusclass.LsCommand.Run();
+        }
+        if (input == "cat")
+        {
+            Console.WriteLine("Type in which file:");
+            string? catInput = Console.ReadLine();
+            magnusclass.CatCommand.Run(catInput);
+        }
+        if (input == "echo")
+        {
+            Console.WriteLine("Type something:");
+            string? echoInput = Console.ReadLine();
+            Console.WriteLine(echoInput);
+        }
 
             if (string.IsNullOrWhiteSpace(input))
                 continue;
@@ -19,20 +35,20 @@ public class Program
             string[] parts = input.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
             string command = parts[0].ToLower();
             string argument = parts.Length > 1 ? parts[1] : "";
-            
-            
+
+
             switch (command)
             {
-            case "pwd":
-                KristofferCLICommands.PwdCommand.Execute();
-                break;
-           case "head":
-                KristofferCLICommands.HeadCommand.Execute(argument);
-                break;
-           case "tail":
-                KristofferCLICommands.TailCommand.Execute(argument);
-                break;
+                case "pwd":
+                    KristofferCLICommands.PwdCommand.Execute();
+                    break;
+                case "head":
+                    KristofferCLICommands.HeadCommand.Execute(argument);
+                    break;
+                case "tail":
+                    KristofferCLICommands.TailCommand.Execute(argument);
+                    break;
             }
-        }
+        }     
     }
 }
--- readme.txt
+++ readme.txt
@@ -6,3 +6,10 @@ magnus tar ls, cat og echo
 kristoffer tar pwd, head og tail
 
 ==================//Commands\\==================
+
+ls - lister ut alle filer og mapper i arbeidskatalogen 
+cat - skriver ut inholdet i en fil
+echo - skriver ut tekst til terminal
+pwd - viser hvilken katalog man befinner seg i 
+head <filnavn> - vis et gitt antall linjer fra starten av en fil
+tail <filnavn> - vis et gitt antall linjer fra slutten av en fil
